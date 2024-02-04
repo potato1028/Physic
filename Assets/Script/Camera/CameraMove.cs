6b8210d931d99af18d5c9bd4070150f21f1abaa7
ghgh10288 <ghgh10288@naver.com>
2024-02-05 03:24:56 +0900

    2/5
    Unity_Flie

---
+++ Assets/Script/Camera/CameraMove.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Cameramove : MonoBehaviour {
+    [Header("Object")]
+    public Transform target;
+
+    public float smoothSpeed = 1.0f;
+
+    void LateUpdate() {
+        if(target != null) {
+            Vector3 desiredPosition = new Vector3(target.position.x, target.position.y + 3f, transform.position.z);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
